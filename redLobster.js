d708e3836a0ea41d6adfb057e39f779e42407263
petrenkorp <edward.petrenko@gmail.com>
2014-10-29 13:52:47 -0400

    Upgrade Search to Directions

--- redLobster.js
+++ redLobster.js
@@ -1,4 +1,8 @@
 var map = document.getElementById("googleMap");
+var latlon;
+var lobsterLocation;
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -8,7 +12,8 @@ function getLocation() {
     }
 }
 function showPosition(position) {
-    var latlon = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	directionsDisplay = new google.maps.DirectionsRenderer();
+    latlon = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 	
 	map.style.height = "400px";
 	map.style.width = "400px";
@@ -21,6 +26,8 @@ function showPosition(position) {
 		navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL}
 	});
 	
+	directionsDisplay.setMap(map);
+	
 	var request = {location: latlon, 
 					radius: 50000,
 					name: "Red Lobster"
@@ -32,11 +39,23 @@ function showPosition(position) {
 
 function callback(results, status){
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
-		var marker = new google.maps.Marker({
-			map: map,
-			position: results[0].geometry.location
-		});
+		lobsterLocation = results[0].geometry.location;
+		displayRoute();
 	}
+}
+
+function displayRoute() {
+	var request = {
+		origin: latlon,
+		destination: lobsterLocation,
+		travelMode: google.maps.TravelMode.DRIVING
+	};
+	
+	directionsService.route(request, function(response, status) {
+		if (status == google.maps.DirectionsStatus.OK) {
+			directionsDisplay.setDirections(response);
+		}
+	});
 }
 	
 function showError(error) {
