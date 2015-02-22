0c17eeb22f1e0eb9bf9a2fd4dd3d1502c7514cf5
Rafik Matta <rafi.matta@gmail.com>
2015-02-21 21:52:53 -0500

    final tox call untested

--- res/toxjs.js
+++ res/toxjs.js
@@ -2,12 +2,38 @@
  * Created by Rafik on 2015-02-21.
  */
 
-var st
-$.post(
-    "http://toxgate.nlm.nih.gov/cgi-bin/sis/search2",
-    function( data )
+
+var resultOfTest = generateChemLink("benzene");
+
+function generateChemLink(chemicalName, callBack)
+{
+    var link;
+
+    $.ajax({
+        type: "POST",
+        url: "http://toxgate.nlm.nih.gov/cgi-bin/sis/search2",
+        data: "queryxxx=" + chemicalName + "&database=hsdb&Stemming=1&" + "and=1&second_search=1&gateway=1&chemsyn=1",
+        success: secondQuery,
+        dataType: "xml"
+    });
+
+
+    function secondQuery(data)
     {
+        var xmlDoc = $.parseXML(data);
+        var tempRecord =  xmlDoc.find("TemporaryFile");
+        $.ajax({
+            url:"http://toxgate.nlm.nih.gov/cgi-bin/sis/search2/g?"+tempRecord +":20",
+            success:returnLink
+        })
 
     }
 
-);
+    function returnLink(data)
+    {
+        var xmlDoc = $.parseXML(data);
+        var docno=  xmlDoc.find("DOCNO");
+        callBack("http://toxgate.nlm.nih.gov/cgi-bin/sis/search2/r?dbs+hsdb:@term+@DOCNO+" + docno);
+    } 
+}
+
